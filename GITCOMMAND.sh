4ffc666f65a338c78e4a958847d021cd5387794d
Antardwip das <antardwip.mtv@gmail.com>
2021-12-06 18:57:10 +0530

    CHANGES

---
+++ GITCOMMAND.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## main commands you need to know in git ##
+## you can execute this file also ./GITCOMMAND.sh in the git shell
+## it will run automatically and execute all this commonds
+
+git add .
+git commit -m "CHANGES"
+git push -u origin main
